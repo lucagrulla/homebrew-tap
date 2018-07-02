ab42aecdd27f5a5fbcb4bc651bf514883d8aaea8
lucagrulla <goreleaser@carlosbecker.com>
2018-07-02 08:03:13 +0100

    Brew formula update for cw version 1.5.0

--- Formula/cw.rb
+++ Formula/cw.rb
@@ -1,9 +1,9 @@
 class Cw < Formula
   desc "The best way to tail AWS Cloudwatch Logs from your terminal"
   homepage "https://www.lucagrulla.com/cw"
-  url "https://github.com/lucagrulla/cw/releases/download/1.4.0/cw_1.4.0_Darwin_x86_64.tar.gz"
-  version "1.4.0"
-  sha256 "37694ad357f6e89eb58360eedb305c4cb13d6cf71820350d53c06d4e53842923"
+  url "https://github.com/lucagrulla/cw/releases/download/1.5.0/cw_1.5.0_Darwin_x86_64.tar.gz"
+  version "1.5.0"
+  sha256 "81b14a3131be30d82d0b662ed9045531b492f430c4fc3ec23aa977b87eedc444"
 
   def install
     bin.install "cw"
