e6d3c424c3d39067fafb1689c03e1ba75313cd08
lucagrulla <goreleaser@carlosbecker.com>
2018-08-06 19:29:57 +0100

    Brew formula update for cw version 1.5.2

--- Formula/cw.rb
+++ Formula/cw.rb
@@ -1,12 +1,18 @@
 class Cw < Formula
   desc "The best way to tail AWS Cloudwatch Logs from your terminal"
   homepage "https://www.lucagrulla.com/cw"
-  url "https://github.com/lucagrulla/cw/releases/download/1.5.1/cw_1.5.1_Darwin_x86_64.tar.gz"
-  version "1.5.1"
-  sha256 "427a48ad345a9f7f13aec377c07618a94a354fbf1d2288342ffa41a60d41df71"
+  url "https://github.com/lucagrulla/cw/releases/download/1.5.2/cw_1.5.2_Darwin_x86_64.tar.gz"
+  version "1.5.2"
+  sha256 "7507766db567658ae3e84aae282f02bff227eb377a64ee15cc0e49c3c14cf94b"
 
   def install
     bin.install "cw"
+    
+    system bin/"cw --completion-script-bash > cw.bash"
+    system bin/"cw completion zsh > cw.zsh"
+    
+    bash_completion.install "cw.bash"
+    zsh_completion.install "cw.zsh"
   end
 
   def caveats; <<~EOS
